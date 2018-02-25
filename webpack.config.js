b6c8af9fa1adaaaa9e46a01a1ffbaa977e9174a5
Burcu Noyan <burcunoyan@gmail.com>
2018-02-25 15:19:47 -0500

    set up webpack and babel presets

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }, {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  }
+}
