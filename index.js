5082f3853b78b449441df772402409511092d482
Burcu Noyan <burcunoyan@gmail.com>
2018-02-25 16:38:42 -0500

    set up initial React components

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
-app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.use('/public', express.static(path.join(__dirname, './public')));
+app.use('/node_modules', express.static(path.join(__dirname, './node_modules')));
+app.use('/dist', express.static(path.join(__dirname, './dist')));
 
 app.get('/*', (req, res, next) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './index.css';
+
+console.log(document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
